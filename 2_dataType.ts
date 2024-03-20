ea6699333b2640b7aa8e2c41dd5bbd8a819e6b84
tanth811 <tanth811@gmail.com>
2024-03-20 17:56:12 +0700

    2.DataType

---
+++ 2_dataType.ts
@@ -0,0 +1,30 @@
+// (Primitve)Basic types: number, string, bool
+// Basic Operators: + - * /
+// Comparison Operators: > < = => <= != !== ===
+
+//number
+console.log("100 + 1")
+console.log(100 + 1)
+console.log(1)
+console.log(-1)
+console.log(1.235)
+console.log(100 + 5 + 6 +1)
+
+//String
+console.log("Hello")
+console.log("1")
+console.log("1+1")
+console.log('Single quote')
+console.log("Double quote")
+console.log(`Back quote`)
+console.log(`Heee`+ `Kradoor`,`Pregnant`)
+console.log("100"+ "5"+"1")
+console.log(100+"5")
+
+//Bool
+console.log(true)
+console.log(false)
+console.log(1 > 0)
+console.log(0 > 1)
+console.log(1 == 1)
+console.log(1 === `1`)
